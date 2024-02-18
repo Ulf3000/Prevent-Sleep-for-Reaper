29cc9695d4b824d9622ef4d85a0704722e40eee8
Alex Mehler <alexmehler@t-online.de>
2024-02-18 11:32:58 +0100

    Update preventsleep.py

--- preventsleep.py
+++ preventsleep.py
@@ -1,15 +1,16 @@
-import ctypes
-from reaper_python import *
+import ctypes               # import to make  dll calls and another bunch of system stuff
+from reaper_python import * # import reaper python api
 
-ES_CONTINUOUS = 0x80000000
-ES_SYSTEM_REQUIRED = 0x00000001
+# https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-setthreadexecutionstate
+#ES_CONTINUOUS = 0x80000000
+#ES_SYSTEM_REQUIRED = 0x00000001
 ES_DISPLAY_REQUIRED = 0x00000002
 
 def deferred_loop(i):
     if RPR_GetPlayState()!= 0: # 0 = stopped; aka on playing, recording and pause
         if i == 100: # count to 100 then it clicks about every 4 to 5 seconds
           i = 0
-          ctypes.windll.kernel32.SetThreadExecutionState(ES_DISPLAY_REQUIRED) # reset the standby timer
+          ctypes.windll.kernel32.SetThreadExecutionState(ES_DISPLAY_REQUIRED) # reset the windows standby timer
           #RPR_ShowConsoleMsg("prevent")
         RPR_defer("deferred_loop("+str(i+1)+")") # i+1
     
