4fc10ba3d3b964a5e4ac7c1579970f4e95ae5cf5
Alex Mehler <alexmehler@t-online.de>
2024-02-18 11:27:50 +0100

    Update preventsleep.py

--- preventsleep.py
+++ preventsleep.py
@@ -1,21 +1,20 @@
 import ctypes
-import reapy_boost as reapy
-
-project = reapy.Project()
+from reaper_python import *
 
 ES_CONTINUOUS = 0x80000000
 ES_SYSTEM_REQUIRED = 0x00000001
 ES_DISPLAY_REQUIRED = 0x00000002
 
 def deferred_loop(i):
-    if project.is_stopped == False: #on playing, recording and pause
-        if i > 100: # count to 100 then it clicks about every 3 seconds
+    if RPR_GetPlayState()!= 0: # 0 = stopped; aka on playing, recording and pause
+        if i == 100: # count to 100 then it clicks about every 4 to 5 seconds
           i = 0
-          # reset the windows standby timer
-          ctypes.windll.kernel32.SetThreadExecutionState(ES_DISPLAY_REQUIRED) 
-        #reapy.print(i)
+          ctypes.windll.kernel32.SetThreadExecutionState(ES_DISPLAY_REQUIRED) # reset the standby timer
+          #RPR_ShowConsoleMsg("prevent")
+        RPR_defer("deferred_loop("+str(i+1)+")")
     
-    # loop
-    reapy.defer(deferred_loop, i + 1)
-
+    else:    # lets not count up to huge numbers please. 
+        RPR_defer("deferred_loop("+str(i)+")")
+        
+    #RPR_ShowConsoleMsg(i)
 deferred_loop(0)  # Start the loop
